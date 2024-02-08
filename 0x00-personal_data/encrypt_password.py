6cbff8f31183c6e10547252dc8efd507ef047da7
TeeJude <teejayjude@gmail.com>
2024-02-08 19:44:46 +0000

    personal_data

---
+++ 0x00-personal_data/encrypt_password.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+"""Module for a hash_password function to return a hashed password"""
+import bcrypt
+from bcrypt import hashpw
+
+
+def hash_password(password: str) -> bytes:
+    """
+    Returns a hashed password
+    Args: 
+        password (str): password to be hashed
+    """
+    b = password.encode()
+    hashed = hashpw(b, bcrypt.gensalt())
+    return hashed
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """
+    Checking whether a password is valid
+    Args:
+        hashed_password (bytes): hashed password
+        password (str): password in string
+    Return:
+        boolean
+    """
+    return bcrypt.checkpw(password.encode(), hashed_password)
